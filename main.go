07692fdd958ff8967427b9c8943b879c0b429442
Eric L. Frederich <eric.frederich@gmail.com>
2013-12-06 16:38:24 -0500

    gofmt

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"os"
-    "time"
-    "log"
     "flag"
     "fmt"
+    "io"
+    "log"
+    "net/http"
+    "os"
+    "time"
 )
 
-func watcher(host, user, password string, interval time.Duration){
+func watcher(host, user, password string, interval time.Duration) {
     url := "http://" + host + "/snapshot.cgi"
     log.Printf("watching(\"%s\", \"%s\", \"%s\")\n", url, user, password)
     i := 0
@@ -25,9 +25,9 @@ func watcher(host, user, password string, interval time.Duration){
         if err != nil {
             panic(err)
         }
-        if resp.StatusCode != 200{
+        if resp.StatusCode != 200 {
             val, exists := resp.Header["Content-Type"]
-            if exists && val[0] == "text/html"{
+            if exists && val[0] == "text/html" {
                 // call log.Fatal with contents of resp.Body
             }
             log.Println("not a 200 response")
@@ -49,10 +49,10 @@ func watcher(host, user, password string, interval time.Duration){
 }
 
 func main() {
-    host     := flag.String  ("h", "ipcam_000000000000_0", "host"     )
-    user     := flag.String  ("u", "admin"               , "user name")
-    password := flag.String  ("p", "admin"               , "password" )
-    interval := flag.Duration("i", 24*time.Second        , "interval" )
+    host := flag.String("h", "ipcam_000000000000_0", "host")
+    user := flag.String("u", "admin", "user name")
+    password := flag.String("p", "admin", "password")
+    interval := flag.Duration("i", 24*time.Second, "interval")
     flag.Parse()
     watcher(*host, *user, *password, *interval)
 }
