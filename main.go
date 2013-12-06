99d9ad7d9eabf4a5a39b33c84a3e8ff12daf6d45
Eric L. Frederich <eric.frederich@gmail.com>
2013-12-06 17:05:39 -0500

    Add total duration so it doesn't run forever / refactor

--- main.go
+++ main.go
@@ -11,45 +11,59 @@ import (
     "time"
 )
 
-func watcher(host, user, password string, interval time.Duration) {
+func grab_picture(url, user, password, fname string){
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        panic(err)
+    }
+    req.SetBasicAuth(user, password)
+
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    if resp.StatusCode != 200 {
+        val, exists := resp.Header["Content-Type"]
+        if exists && val[0] == "text/html" {
+            b, err := ioutil.ReadAll(resp.Body)
+            if err != nil {
+                panic(err)
+            }
+            log.Fatal(string(b))
+        }
+        log.Println("not a 200 response")
+    }
+
+    f, err := os.Create(fname)
+    if err != nil {
+        panic(err)
+    }
+
+    written, err := io.Copy(f, resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    log.Printf("Wrote %s (%d bytes)\n", fname, written)
+}
+
+func watcher(host, user, password string, interval, duration time.Duration) {
+
     url := "http://" + host + "/snapshot.cgi"
     log.Printf("watching(\"%s\", \"%s\", \"%s\")\n", url, user, password)
+
+    ticker := time.Tick(interval)
+    done   := time.After(duration)
     i := 0
-    for _ = range time.Tick(interval) {
-        req, err := http.NewRequest("GET", url, nil)
-        if err != nil {
-            panic(err)
+    for {
+        select{
+        case <-ticker:
+            fname := fmt.Sprintf("out_%05d.jpg", i)
+            grab_picture(url, user, password, fname)
+            i += 1
+        case <-done:
+            return
         }
-        req.SetBasicAuth(user, password)
-
-        resp, err := http.DefaultClient.Do(req)
-        if err != nil {
-            panic(err)
-        }
-        if resp.StatusCode != 200 {
-            val, exists := resp.Header["Content-Type"]
-            if exists && val[0] == "text/html" {
-                b, err := ioutil.ReadAll(resp.Body)
-                if err != nil {
-                    panic(err)
-                }
-                log.Fatal(string(b))
-            }
-            log.Println("not a 200 response")
-        }
-
-        fname := fmt.Sprintf("out_%05d.jpg", i)
-        f, err := os.Create(fname)
-        if err != nil {
-            panic(err)
-        }
-
-        written, err := io.Copy(f, resp.Body)
-        if err != nil {
-            panic(err)
-        }
-        log.Printf("Wrote %s (%d bytes)\n", fname, written)
-        i += 1
     }
 }
 
@@ -58,6 +72,7 @@ func main() {
     user := flag.String("u", "admin", "user name")
     password := flag.String("p", "admin", "password")
     interval := flag.Duration("i", 24*time.Second, "interval")
+    duration := flag.Duration("d", 12*time.Hour  , "duration")
     flag.Parse()
-    watcher(*host, *user, *password, *interval)
+    watcher(*host, *user, *password, *interval, *duration)
 }
